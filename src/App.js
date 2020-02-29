ebdbf6be688b835cab66b07a6c6bbb4465f75ebe
Bhupendra <bhupendra1011@gmail.com>
2020-02-29 20:18:46 +0530

    initial-config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
+
+// add todos
+// delete todos
+// update todos
+// fetch todos
 
 function App() {
+  const { loading, error, data } = useQuery(GET_TODOS);
+  if (loading) return <div>Loading Todos....</div>;
+  if (error) return <div>Error fetching Todos...</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      {data.todos.map(todo => (
+        <p key={todo.id}>
+          <span>{todo.text}</span>
+          <button>&times;</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// setting up client
+const client = new ApolloClient({
+  uri: "https://saxo-todo.herokuapp.com/v1/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
